e992e11e792ef4364d2065f54da4aefbae4e66e0
marrosuksw <marcelrossi03@gmail.com>
2023-12-07 12:14:30 +0100

    first commit

---
+++ Project34/Visitor.h
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include<array>
+
+class ConcreteComponentA;
+class ConcreteComponentB;
+
+class Visitor {
+public:
+    virtual void VisitConcreteComponentA(const ConcreteComponentA* element) const = 0;
+    virtual void VisitConcreteComponentB(const ConcreteComponentB* element) const = 0;
+};
